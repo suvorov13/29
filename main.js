b6861f29d3d97bd53e30b4439f64a733709b3488
AndreyAnd7 <92207370+AndreyAnd7@users.noreply.github.com>
2022-01-15 19:55:14 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,225 @@
+// let a = prompt('a = ')
+// let b = prompt('b = ')
+// let c = prompt('c = ')
+
+// let p = a*1+b*1+c*1
+
+// alert(`p = ${p}`)
+
+// let a = prompt('a = ')
+
+// let S = a*a
+
+// alert(`S = ${S}`)
+
+// let a = prompt('a = ')
+
+// let p = a*4
+
+// alert(`p = ${p}`)
+
+// let a = prompt('a = ')
+// let b = prompt('b = ')
+
+// let s = a*b
+// let p = 2*(a*1+b*1)
+
+// alert(`s = ${s}`)
+// alert(`p = ${p}`)
+
+// let d = prompt('d = ')
+// const pi = 3.14
+
+// let L = d*pi
+
+// alert(`L = ${L}`)
+
+// let a = prompt('a = ')
+// let v = a*a*a
+// let s = 6*a*a
+
+// alert(`v = ${v}`)
+// alert(`s = ${s}`)
+
+// let a = prompt('a = ')
+// let b = prompt('b = ')
+// let c = prompt('c = ')
+
+// let v = a*b*c
+// let s = 2*(a*b+b*c+a*c)
+
+// alert(`v = ${v}`)
+// alert(`s = ${s}`)
+
+// let r = prompt('r = ')
+// const p = 3.14
+// let l = 2*p*r
+// let s = p*r*r
+// alert(`l = ${l}`)
+// alert(`s = ${s}`)
+
+// let a = prompt('a = ')
+// let b = prompt('b = ')
+// let result = (a*1+b*1)/2
+
+// alert(`result = ${result}`)
+
+// let a = prompt('a = ')
+// let b = prompt('b = ')
+
+// let result = Math.sqrt(a*b)
+
+// alert(`result = ${result}`)
+
+// let a = prompt('a = ')
+// let b = prompt('b = ')
+
+// let ka = a*a
+// let kb = b*b
+
+// let sum = ka*1+kb*1
+// let min = ka*1-kb*1
+// let x = ka*kb
+// let y = ka/kb
+
+// alert(`result = ${sum}`)
+// alert(`result = ${min}`)
+// alert(`result = ${x}`)
+// alert(`result = ${y}`)
+
+// let a = prompt('a = ')
+// let b = prompt('b = ')
+
+// let ma = Math.abs(a)
+// let mb = Math.abs(b)
+
+// let sum = ma*1+mb*1
+// let min = ma*1-mb*1
+// let x = ma*mb
+// let y = ma/mb
+
+// alert(`result = ${sum}`)
+// alert(`result = ${min}`)
+// alert(`result = ${x}`)
+// alert(`result = ${y}`)
+
+// let a = prompt('a = ')
+// let b = prompt('b = ')
+
+// let c = Math.sqrt(a*a+b*b)
+// let p = a*1+b*1+c*1
+
+// alert(`c = ${c}`)
+// alert(`p = ${p}`)
+
+// let a = 'aadda'
+// let b = 123
+// let c = 125n
+// let d = false
+// let e
+// let f = null
+// let g = '45' - '20'
+// let h = '12345'
+
+// alert(typeof Number(a))
+// alert(typeof String(b))
+// alert(typeof String(c))
+// alert(typeof (d))
+// alert(typeof (e))
+// alert(typeof (f))
+// alert(typeof (g))
+// alert(typeof (h))
+
+// let r1 = prompt('r1 = ')
+// let r2 = prompt('r2 = ')
+// let p = 3.14
+// let s1 = p*r1*r1
+// let s2 = p*r2*r2
+// let s3 = s1*1-s2*1
+
+// alert(`s1 = ${s1}`)
+// alert(`s2 = ${s2}`)
+// alert(`s3 = ${s3}`)
+
+// let l = prompt('l = ')
+// let p = 3.14
+// let r = l/(2*p)
+// let s = p*r*r
+
+// alert(`r = ${r}`)
+// alert(`s = ${s}`)
+
+// let s = prompt('s = ')
+// let p = 3.14
+// let d = Math.sqrt(s/p*4)
+// let l = d*p
+
+// // alert(`d = ${d}`)
+// // alert(`l = ${l}`)
+
+// let x1 = prompt('x1 = ')
+// let x2 = prompt('x2 = ')
+
+// let result = Math.abs(x1*1-x2*1)
+
+// alert(`result = ${result}`)
+
+// let l = prompt('l = ')
+// let sm = Math.floor(l/100)
+// alert(`result = ${sm}`)
+
+// let m = prompt('m = ')
+// let kg = Math.floor(m/1000)
+// alert(`result = ${kg}`)
+
+// let b = prompt('b = ')
+// let kb = Math.floor(b/1024)
+// alert(`result = ${kb}`)
+
+// let a = prompt('a = ')
+// let b = prompt('b = ')
+// let result = Math.floor(a/b)
+// alert(`result = ${result}`)
+
+// let a = prompt('a = ')
+// let b = prompt('b = ')
+// let result = a%b
+// alert(`result = ${result}`)
+
+// let a = prompt('a = ')
+// let b = prompt('b = ')
+// let x10 = a/b
+// let x1 = a%b
+// alert(`x10 = ${x10}`)
+// alert(`x1 = ${x1}`)
+
+// let a = prompt('a = ')
+// let x10 =Math.floor(a/10) 
+// let x1 = a%10
+// let sum = x10+x1
+// alert(`suma = ${sum}`)
+
+// let a = prompt('a = ')
+// let x10 =Math.floor(a/10) 
+// let x1 = a%10
+// let b = x1*10+x10
+// alert(`b = ${b}`)
+
+// let a = prompt('a = ')
+// let b = Math.floor(a/100)
+// alert(`b = ${b}`)
+
+// let a = prompt('a = ')
+// let b = a%10
+// let c = Math.floor((a%100)/10)
+// alert(`b = ${b}`)
+// alert(`c = ${c}`)
+
+let a = prompt('a = ')
+let d = Math.floor(a/100)
+let b = a%10
+let c = Math.floor((a%100)/10)
+let sum = b*1+c*1+d*1
+let x = b*c*d
+alert(`sum = ${sum}`)
+alert(`x = ${x}`)
